1eea194ce8435cdef8c83a427ac7b73906f410f1
Angel Resendiz <angel.jrl@hotmail.com>
2023-03-15 08:46:14 -0600

    se agrego servicio de api

---
+++ src/app/services/movies.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MoviesService {
+
+  constructor( private http: HttpClient ) { }
+
+  getFeature(){
+    return this.http.get('https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2000-09-15&primary_release_date.lte=2014-10-22&api_key=0eae98972653d037575a7cab39ef9b5d&language=es&include_image_language=es');
+  }
+}

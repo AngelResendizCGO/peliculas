03bbb698a1602947243a954638720fcade367b2d
Angel Resendiz <angel.jrl@hotmail.com>
2023-03-16 10:09:01 -0600

    se agrego imagenes en el slide con tamanno w500

--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { RespuestaMDB } from '../interfaces/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,6 @@ export class MoviesService {
   constructor( private http: HttpClient ) { }
 
   getFeature(){
-    return this.http.get('https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2000-09-15&primary_release_date.lte=2014-10-22&api_key=0eae98972653d037575a7cab39ef9b5d&language=es&include_image_language=es');
+    return this.http.get<RespuestaMDB>('https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2000-09-15&primary_release_date.lte=2014-10-22&api_key=0eae98972653d037575a7cab39ef9b5d&language=es&include_image_language=es');
   }
 }
